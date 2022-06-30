9da0e240ce5ed90c71e7753103a6c145b9e01c10
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-06-29 21:57:18 -0300

    Adding places show endpoint

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::PlacesController < Api::V1::BaseController
+  before_action :set_place, only: [:show]
 
   def index
     @places = policy_scope(Place)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+    authorize @place
   end
 end
--- app/policies/place_policy.rb
+++ app/policies/place_policy.rb
@@ -9,4 +9,8 @@ class PlacePolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    true
+  end
 end
